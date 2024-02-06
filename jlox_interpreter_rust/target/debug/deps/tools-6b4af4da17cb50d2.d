6d8dbfddeb0c742b9527c820b37673b96746af3b
Michael Barakat <michaelbkats@gmail.com>
2024-02-05 22:10:42 -0800

    Delete jlox_interpreter_rust/target directory

--- jlox_interpreter_rust/target/debug/deps/tools-6b4af4da17cb50d2.d
+++
@@ -1,5 +0,0 @@
-/Users/michaelbarakat/projects/crafting interpreters/jlox_interpreter_rust/target/debug/deps/tools-6b4af4da17cb50d2: tools/src/main.rs
-
-/Users/michaelbarakat/projects/crafting interpreters/jlox_interpreter_rust/target/debug/deps/tools-6b4af4da17cb50d2.d: tools/src/main.rs
-
-tools/src/main.rs:
--- jlox_interpreter_rust/target/debug/tools.d
+++
@@ -1,1 +0,0 @@
-/Users/michaelbarakat/projects/crafting\ interpreters/jlox_interpreter_rust/target/debug/tools: /Users/michaelbarakat/projects/crafting\ interpreters/jlox_interpreter_rust/tools/src/main.rs
