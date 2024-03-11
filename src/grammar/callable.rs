e3388fd38e3aafe3e71a40c624f14fd171412c6c
mikeybkats <michaelbkats@gmail.com>
2024-03-10 20:41:30 -0700

    removing LoxNativeFunction

--- src/grammar/callable.rs
+++ src/grammar/callable.rs
@@ -5,7 +5,6 @@ use super::object::Object;
 #[derive(Debug, Clone)]
 pub enum Callable {
     LoxFunction(LoxFunction),
-    _LoxNativeFunction(LoxNativeFunction),
     Clock(Clock),
 }
 
@@ -17,7 +16,6 @@ impl Callable {
     ) -> Result<Object, RuntimeError> {
         match self {
             Callable::LoxFunction(f) => f.call(interpreter, arguments),
-            Callable::_LoxNativeFunction(f) => f.call(interpreter, arguments),
             Callable::Clock(c) => c.call(interpreter, arguments),
         }
     }
@@ -29,27 +27,8 @@ pub trait LoxCallable<T> {
     fn arity(&self) -> u8;
 }
 
-/// # LoxNativeFunction
+/// # Clock - LoxNativeFunction
 /// Crafting Interpreters 10.2 - "These are functions that the interpreter exposes to user code but that are implemented in the host language (in our case Java), not the language being implemented (Lox)."
-#[derive(Debug, Clone)]
-pub struct LoxNativeFunction {
-    pub arity: u8,
-}
-impl LoxCallable<Result<Object, RuntimeError>> for LoxNativeFunction {
-    fn arity(&self) -> u8 {
-        self.arity
-    }
-
-    fn call(
-        &self,
-        _interpreter: &mut Interpreter,
-        _arguments: Vec<Object>,
-    ) -> Result<Object, RuntimeError> {
-        // TODO: Implement LoxFunction::call
-        Ok(Object::Nil)
-    }
-}
-
 #[derive(Debug, Clone)]
 pub struct Clock;
 impl Clock {
