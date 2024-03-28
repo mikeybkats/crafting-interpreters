19514fce0f100cac15a9c27bc41e6d25f99b096d
mikeybkats <michaelbkats@gmail.com>
2024-03-28 07:07:26 -0700

    Adding declare to resolver impl

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
+
 use crate::{
     error::LoxError,
     grammar::{
         expr::{Expr, ExprVisitor},
         object::Object,
-        stmt::{FunStmt, StmtVisitor},
+        stmt::{BlockStmt, FunStmt, Stmt, StmtVisitor},
         token::Token,
     },
     interpreter::Interpreter,
@@ -14,11 +16,59 @@ use crate::{
 /// The resolver visits every node in the syntax tree and resolves the scope of each variable.
 ///
 /// It helps optimize the code by resolving variable scopes at compile time.
-struct Resolver {
+struct Resolver<'a> {
     interpreter: Interpreter,
+    scopes: Vec<&'a mut HashMap<String, bool>>,
 }
 
-impl ExprVisitor<Result<Object, LoxError>> for Resolver {
+impl Resolver<'_> {
+    pub fn new(interpreter: Interpreter) -> Self {
+        Self {
+            interpreter,
+            scopes: Vec::new(),
+        }
+    }
+
+    fn begin_scope(&mut self) {
+        unimplemented!()
+    }
+
+    fn resolve(&mut self, statements: &mut Vec<Stmt>) -> Result<Object, LoxError> {
+        for statement in statements {
+            self.resolve_stmt(statement)?;
+        }
+
+        Ok(Object::Nil)
+    }
+
+    fn resolve_stmt(&mut self, statement: &mut Stmt) -> Result<Object, LoxError> {
+        statement.accept(self)
+    }
+
+    fn resolve_expr(&mut self, expr: &Expr) -> Result<Object, LoxError> {
+        expr.accept(self)
+    }
+
+    fn end_scope(&mut self) {
+        self.scopes.pop();
+    }
+
+    /// # Declare
+    /// as we visit expressions, we need to know if we’re inside the initializer for some variable. We do that by splitting binding into two steps. The first is declaring it.
+    fn declare(&mut self, name: &Token) {
+        if self.scopes.is_empty() {
+            return;
+        }
+
+        let scope = self.scopes.last_mut();
+
+        if let Some(scope) = scope {
+            scope.insert(name.lexeme.clone(), false);
+        }
+    }
+}
+
+impl ExprVisitor<Result<Object, LoxError>> for Resolver<'_> {
     fn visit_binary_expr(
         &mut self,
         _left: &Expr,
@@ -67,9 +117,9 @@ impl ExprVisitor<Result<Object, LoxError>> for Resolver {
     }
 }
 
-impl StmtVisitor<Result<Object, LoxError>> for Resolver {
-    fn visit_expression_stmt(&mut self, _stmt: &Expr) -> Result<Object, LoxError> {
-        unimplemented!()
+impl StmtVisitor<Result<Object, LoxError>> for Resolver<'_> {
+    fn visit_expression_stmt(&mut self, stmt: &Expr) -> Result<Object, LoxError> {
+        stmt.accept(self)
     }
 
     fn visit_function_stmt(&mut self, _fun_stmt: &mut FunStmt) -> Result<Object, LoxError> {
@@ -96,23 +146,21 @@ impl StmtVisitor<Result<Object, LoxError>> for Resolver {
     fn visit_while_stmt(
         &mut self,
         _condition: &Expr,
-        _body: &mut crate::grammar::stmt::Stmt,
+        _body: &mut Stmt,
     ) -> Result<Object, LoxError> {
         unimplemented!()
     }
 
-    fn visit_var_stmt(
-        &mut self,
-        _name: &crate::grammar::token::Token,
-        _initializer: &Expr,
-    ) -> Result<Object, LoxError> {
-        unimplemented!()
+    fn visit_var_stmt(&mut self, name: &Token, initializer: &Expr) -> Result<Object, LoxError> {
+        self.declare(name);
+        self.resolve_expr(initializer)
     }
 
-    fn visit_block_stmt(
-        &mut self,
-        _statements: &mut crate::grammar::stmt::BlockStmt,
-    ) -> Result<Object, LoxError> {
-        unimplemented!()
+    fn visit_block_stmt(&mut self, statements: &mut BlockStmt) -> Result<Object, LoxError> {
+        self.begin_scope();
+        let result = self.resolve(&mut statements.statements);
+        self.end_scope();
+
+        result
     }
 }
