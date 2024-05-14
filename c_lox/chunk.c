3bb0064c43764bc8a40255b8c6287f490afe9f16
mikeybkats <michaelbkats@gmail.com>
2024-05-14 06:54:17 -0700

    adding bones

---
+++ c_lox/chunk.c
@@ -0,0 +1,23 @@
+#include "chunk.h"
+
+#include <stdlib.h>
+
+void initChunk(Chunk* chunk) {
+  chunk->count = 0;
+  chunk->capacity = 0;
+  chunk->code = NULL;
+}
+
+void writeChunk(Chunk* chunk, uint8_t byte) {
+  // check to see if the array has adequate capacity
+  if (chunk->capacity < chunk->count + 1) {
+    // if it does not, then grown the array
+    int oldCapacity = chunk->capacity;
+    chunk->capacity = GROW_CAPACITY(oldCapacity);
+    chunk->code =
+        GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+  }
+
+  chunk->code[chunk->capacity] = byte;
+  chunk->count++;
+}
