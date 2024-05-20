5dac71372520a797f93a9d8436e9238f538f2fb8
mikeybkats <michaelbkats@gmail.com>
2024-05-20 07:00:30 -0700

    adding testing setup. not working right

---
+++ c_lox/tests/test_main.c
@@ -0,0 +1,16 @@
+#include "tests/unity/src/unity.h"
+
+void setUp(void) { /* General initialization */ }
+void tearDown(void) { /* General cleanup */ }
+
+void test_function2(void) {
+  int val = 4;
+  TEST_ASSERT_EQUAL_INT_MESSAGE(5, val, "Not five? Not alive!");
+}
+
+int main(void) {
+  // implement tests
+  UNITY_BEGIN();
+  RUN_TEST(test_function2);
+  return UNITY_END();
+}
