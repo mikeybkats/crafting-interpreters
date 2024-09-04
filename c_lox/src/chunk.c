06cbdb43a37c8d7592a457006db0ab3fc0dd6808
mikeybkats <michaelbkats@gmail.com>
2024-09-04 06:18:21 -0700

    adding old changes

--- c_lox/src/chunk.c
+++ c_lox/src/chunk.c
@@ -52,6 +52,20 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
 
 /*
  * ## writeRleChunk
+ *
+ * Our encoding of line information is hilariously wasteful of memory. Given
+ * that a series of instructions often correspond to the same source line, a
+ * natural solution is something akin to run-length encoding of the line
+ * numbers.
+ *
+ * Devise an encoding that compresses the line information for a series of
+ * instructions on the same line. Change writeChunk() to write this compressed
+ * form, and implement a getLine() function that, given the index of an
+ * instruction, determines the line where the instruction occurs.
+ *
+ * Hint: It’s not necessary for getLine() to be particularly efficient. Since it
+ * is called only when a runtime error occurs, it is well off the critical path
+ * where performance matters.
  */
 void writeRleChunk(Chunk* chunk, uint8_t byte, int line) {
   // check to see if the array has adequate capacity
