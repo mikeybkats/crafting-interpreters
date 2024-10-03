b48def4abfc8cadeb117c50dbd2c903302d6b2aa
mikeybkats <michaelbkats@gmail.com>
2024-10-03 16:10:23 -0700

    Adding code for 16.2 - fixes #72

--- c_lox/src/compiler.c
+++ c_lox/src/compiler.c
@@ -7,4 +7,20 @@
 
 void compile(const char* source) {
   initScanner(source);
+
+  int line = -1;
+
+  // "This loops indefinitely. Each turn through the loop, it scans one token and prints it. When it reaches a special “end of file” token or an error, it stops. For example, if we run the interpreter on this program:"
+  for (;;) {
+    Token token = scanToken();
+    if (token.line != line) {
+      printf("%4d ", token.line);
+      line = token.line;
+    } else {
+      printf("  | ");
+    }
+    printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+    if (token.type == TOKEN_EOF) break;
+  }
 }
