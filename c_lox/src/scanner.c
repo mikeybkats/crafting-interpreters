d7dc9b794c3099308619e0d69a0750249e93cf27
mikeybkats <michaelbkats@gmail.com>
2024-10-07 06:58:57 -0700

    adding 16.3.2

--- c_lox/src/scanner.c
+++ c_lox/src/scanner.c
@@ -33,6 +33,11 @@ static char peek() {
   return *scanner.current;
 }
 
+static char peekNext() {
+  if (isAtEnd()) return '\0';
+  return scanner.current[1];
+}
+
 static bool match(char expected) {
   if (isAtEnd()) return false;
 
@@ -78,6 +83,14 @@ static void skipWhitespace() {
       case '\r':
       case '\t':
         advance();
+        break;
+      case '/':
+        if (peekNext() == '/') {
+          // A comment goes until the end of the line.
+          while (peek() != '\n' && !isAtEnd()) advance();
+        } else {
+          return;
+        }
         break;
       default:
         return;
