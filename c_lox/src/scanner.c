c5b49863fd7354859969c2873dc8509ef9b2bf29
mikeybkats <michaelbkats@gmail.com>
2024-10-07 06:53:01 -0700

    adding 16.3

--- c_lox/src/scanner.c
+++ c_lox/src/scanner.c
@@ -24,6 +24,21 @@ static bool isAtEnd() {
   return *scanner.current == "\0";
 }
 
+static char advance() {
+  scanner.current++;
+  return scanner.current[-1];
+}
+
+static bool match(char expected) {
+  if (isAtEnd()) return false;
+
+  // if current char == expected return true
+  if (*scanner.current != expected) return false;
+  scanner.current++;
+
+  return true;
+}
+
 /*
 ## makeToken
 
@@ -56,7 +71,44 @@ Token scanToken() {
 
   if (isAtEnd()) return makeToken(TOKEN_EOF);
 
-  // do stuff here
+  char c = advance();
+
+  switch (c) {
+    case '(':
+      return makeToken(TOKEN_LEFT_PAREN);
+    case ')':
+      return makeToken(TOKEN_RIGHT_PAREN);
+    case '{':
+      return makeToken(TOKEN_LEFT_BRACE);
+    case '}':
+      return makeToken(TOKEN_RIGHT_BRACE);
+    case ';':
+      return makeToken(TOKEN_SEMICOLON);
+    case ',':
+      return makeToken(TOKEN_COMMA);
+    case '.':
+      return makeToken(TOKEN_DOT);
+    case '-':
+      return makeToken(TOKEN_MINUS);
+    case '+':
+      return makeToken(TOKEN_PLUS);
+    case '/':
+      return makeToken(TOKEN_SLASH);
+    case '*':
+      return makeToken(TOKEN_STAR);
+    case '!':
+      return makeToken(
+          match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+    case '=':
+      return makeToken(
+          match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+    case '<':
+      return makeToken(
+          match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+    case '>':
+      return makeToken(
+          match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+  }
 
   return errorToken("unexpected character.");
 }
