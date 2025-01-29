f422c0a8db93e71d11f843b159ec8aa067897966
mikeybkats <michaelbkats@gmail.com>
2025-01-29 07:02:14 -0800

    adding table

--- c_lox/src/table.c
+++ c_lox/src/table.c
@@ -7,6 +7,8 @@
 #include "object.h"
 #include "value.h"
 
+#define TABLE_MAX_LOAD 0.75  // the table should grow when capacity reaches 75%
+
 void initTable(Table* table) {
   table->count    = 0;
   table->capacity = 0;
@@ -16,4 +18,45 @@ void initTable(Table* table) {
 void freeTable(Table* table) {
   FREE_ARRAY(Entry, table->entries, table->capacity);
   initTable(table);
+}
+
+static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
+  // map key's hash code to an index within the array bounds
+  uint32_t index = key->hash % capacity;
+
+  for (;;) {
+    // get the entry by index
+    Entry* entry = &entries[index];
+
+    // if the entry key equals the find key
+    // or
+    // if the entry key is NULL (the bucket contains no entry)
+    if (entry->key == key || entry->key == NULL) {
+      // entry found, return the entry
+      return entry;
+    }
+
+    index = (index + 1) % capacity;
+  }
+}
+
+bool tableSet(Table* table, ObjString* key, Value value) {
+  // check to make sure the entry can fit
+  if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+    int capacity = GROW_CAPACITY(table->capacity);
+    adjustCapacity(table, capacity);
+  }
+
+  // find a bucket for the entry
+  Entry* entry = findEntry(table->entries, table->capacity, key);
+
+  // update the size of the table
+  bool isNewKey = entry->key == NULL;
+  if (isNewKey) table->count++;
+
+  // copy the entry into the table
+  entry->key   = key;
+  entry->value = value;
+
+  return isNewKey;
 }
--- c_lox/src/table.h
+++ c_lox/src/table.h
@@ -20,4 +20,6 @@ typedef struct
 void initTable(Table* table);
 void freeTable(Table* table);
 
+bool tableSet(Table* table, ObjString* key, Value value);
+
 #endif
