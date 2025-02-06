a5c147677a3759ec32c39840f3053cb5335401b5
mikeybkats <michaelbkats@gmail.com>
2025-02-06 06:44:00 -0800

    finishing ch20 challenge 1

--- c_lox/src/table.h
+++ c_lox/src/table.h
@@ -26,14 +26,17 @@ typedef struct
 void initTable(Table* table);
 void freeTable(Table* table);
 
+#ifdef DEBUG_TEST
 STATIC Entry* findEntry(Entry* entries, int capacity, Value* key);
-bool          tableSet(Table* table, Value* key, Value* value);
-bool          tableDelete(Table* table, Value* key);
-bool          tableGet(Table* table, Value* key, Value* value);
-void          tableAddAll(Table* from, Table* to);
-ObjString*    tableFindString(Table* table, const char* chars, int length, uint32_t hash);
-uint32_t      getHashValue(Value value);
-int           getEntryIndex(Table* table, Value* key);
-bool          isTombstone(Entry* entry);
+#endif
+
+bool       tableSet(Table* table, Value* key, Value* value);
+bool       tableDelete(Table* table, Value* key);
+bool       tableGet(Table* table, Value* key, Value* value);
+void       tableAddAll(Table* from, Table* to);
+ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t hash);
+uint32_t   getHashValue(Value value);
+int        getEntryIndex(Table* table, Value* key);
+bool       isTombstone(Entry* entry);
 
 #endif
